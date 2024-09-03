a64cf70b21ead20e0f95658a12f5d06bf46085ba
NataliaTorres5 <nataliatorres88@gmail.com>
2024-09-02 20:46:43 -0500

    se crearon las rutas de users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router();  
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const {create} = require('../controllers/userController')//esto define un objeto se le define un metodo para qu elo controle el controlador
+
+
+router.post('/', create) 
 
 module.exports = router;

081b9bdd12f516c6f54754f5738ef73aa6d42619
NataliaTorres5 <nataliatorres88@gmail.com>
2024-09-02 20:46:23 -0500

    se crearon las rutas de blog y users

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const blogsRouter =require('./blogs') //se esta requiriendo las rutas particulares con la /se llama tods las rutas definidas dentro del enrutador
+const usersRouter =require('./users')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json();
+  res.render('index', {title: 'Caribdis Ecommerce'});
 });
 
+//se define otra ruta 
+router.use('/blogs', blogsRouter)
+router.use('/users', usersRouter)
 
 
-module.exports = router;
 
-/*
-En el caso de que se detecte que el id no existe se debe poner un aviso de esta manera
-
-router.get('/', function(req, res, next) {
- if  (req.params.id )  {
- res.status(404).json() 
- } 
- res.json({
- id : req.para,s.id });
-});
- */ 
+module.exports = router; //enrutador principal de la app que tiene que usar todos los controladres
